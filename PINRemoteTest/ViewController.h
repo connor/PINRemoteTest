7b7dfa33f91e3e79ba4331dfbd79733df8ea0789
Kerr Marin Miller <kerr.miller@net-a-porter.com>
2015-09-30 11:43:00 +0100

    Initial Commit

---
+++ PINRemoteTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PINRemoteTest
+//
+//  Created by Kerr Miller on 30/09/2015.
+//  Copyright © 2015 Kerr Marin Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PINRemoteTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PINRemoteTest
+//
+//  Created by Kerr Miller on 30/09/2015.
+//  Copyright © 2015 Kerr Marin Miller. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
